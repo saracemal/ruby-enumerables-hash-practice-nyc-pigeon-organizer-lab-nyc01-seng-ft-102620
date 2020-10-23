bf2da093b27359696c07b05108c214c1c9d2ce47
Sara Cemal <saracemal@gmail.com>
2020-10-23 04:55:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,17 @@
 def nyc_pigeon_organizer(data)
-  final_results = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, names|
-      names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
+  pigeon_list = {}
+  data.each do |color_gender_lives, value|
+    value.each do |stats, all_names|
+      all_names.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
         end
-        if !final_array[name][key]
-          final_array[name][key] = []
+        if pigeon_list[name][color_gender_lives] == nil
+          pigeon_list[name][color_gender_lives] = []
         end
-        final_array[name][key].push(inner.key.to_s)
+        pigeon_list[name][color_gender_lives].push(stats.to_s)
       end
-    end
+    end 
   end
+  pigeon_list
 end
